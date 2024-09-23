0c767fa5f80fee6e4553e764f0346f3437cf41c5
jeffszo <jefersonferreira27@outlook.com>
2024-09-23 14:33:49 -0300

    remove export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import {Poppins} from "next/font/google"
 import "./globals.css";
 
 
-export const poppins = Poppins({
+const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100','400','500', '600', '700', '800', '900']
 })
