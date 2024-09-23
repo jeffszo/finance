ec7028dc2ff9ddcb74050d0fe3acf87b37c19d5f
jeffszo <jefersonferreira27@outlook.com>
2024-09-23 14:40:05 -0300

    resolve error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import {Poppins} from "next/font/google"
+import { Poppins } from "next/font/google";
 import "./globals.css";
-
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['100','400','500', '600', '700', '800', '900']
-})
+  weight: ['100', '400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
   title: "Finance",
@@ -15,9 +14,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-br">
       <body className={poppins.className}>
